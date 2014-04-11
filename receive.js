5097efc9a04fa310caf164d7bd28e7249680673c
Yuichi M <yuichimaki.0124@gmail.com>
2014-04-11 20:08:10 +0900

    sensorServerとのwsClient，clientとのwsServerの作成

--- receive.js
+++ receive.js
@@ -1,18 +1,58 @@
-var WebSocket = require('ws');
+var fs = require('fs');
+var WsClient = require('ws');
+var WsServer = require('ws').Server;
 
-var HOST = '127.0.0.1';
-var PORT = '8080';
+var SENSOR_HOST = '192.168.1.3';
+var SENSOR_PORT = '8000';
+var NODE_HOST = '192.168.1.4';
+var NODE_PORT = '8080';
 
-var ws = new WebSocket('ws://' + HOST + ':' + PORT + '/');
+var http = require('http').createServer(function(req, res){
+    fs.readFile(__dirname + '/index.html', function(err, data){
+	if(err){
+	    res.writeHead(500);
+	    console.log("Internal Server Error");
+	} else {
+	    try {
+		res.writeHead(200);
+		res.end(data);
+	    } catch(e){
+		console.log("Internal Server Error");
+		res.end("Internal Server Error");
+	    }
+	}
+    });
+}).listen(NODE_PORT, function(){
+    console.log((new Date()) + ": httpServer is Listening @" + NODE_HOST + " on port " + NODE_PORT + ". ");
+});
 
-ws.on('open', function(){   
-	console.log("Connection Opened!");
+var wsClient = new WsClient('ws://' + SENSOR_HOST + ':' + SENSOR_PORT + '/'); // node - sensorServer : new
+var wsServer = new WsServer({host:NODE_HOST, port:NODE_PORT}); // client - node : new
+
+wsServer.on('connection', function(ws){ // client - node : connect
+    ws.on('message', function(message){
+	// do something when client send some msg.
     });
 
-ws.on('message', function(data){
-	var buf = JSON.parse(data);
-	if(buf.data.length !== 0){
-	    for(var i = 0; i < buf.data.length; i++){
+    ws.on('close', function(){
+	// do something when client - node is closed.
+    });
+});
+
+wsClient.on('open',function(){ // node - sensorServer : open
+    console.log("node is connected to sensorServer");
+    wsClient.send('node is connected to sensorServer', {mask:true});
+});
+
+var counter = 0;
+wsClient.on('message', function(data){ // node - sensorServer : message
+    var buf = JSON.parse(data); 
+    console.log("<><><><><><><><><><><><>:" + counter);
+    counter++; // for Debug
+    if(buf.data.length !== 0){
+	for(var i = 0; i < buf.data.length; i++){
+	    if(4031 == buf.data[i][1]){ // ID authentification
+		/*
 		console.log("******************");
 		console.log("dateTime: " + buf.data[i][0]);
 		console.log("ID: " + buf.data[i][1]);
@@ -22,13 +62,15 @@ ws.on('message', function(data){
 		console.log("ax: " + buf.data[i][5]);
 		console.log("ay: " + buf.data[i][6]);
 		console.log("az: " + buf.data[i][7]);
-		console.log("******************");
+		*/
+		counter = 0;
 	    }
-	    
-	}
-    });
+	}	    
+    }    
+});
 
-ws.on('close', function(){
-	       console.log("Connection closed!");
-	   });
+// node - sensorServer : close
+wsClient.on('close', function(){
+    console.log("Connection closed!");
+});
   
